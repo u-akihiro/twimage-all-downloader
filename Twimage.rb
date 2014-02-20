98280510fd620f51dc7738779fe60d6936ea650f
akihiro <akihiro.uesaka@gmail.com>
2014-02-20 14:38:09 +0900

    まだ修正中

--- Twimage.rb
+++ Twimage.rb
@@ -14,9 +14,16 @@ class Twimage
 		content_type = response['content-type'].split(';')[0]
 		#HTMLとJSONで処理を分ける
 		if content_type == 'text/html' do
-			
+			html = Nokogiri::HTML(request.body)
+			spans = html.xpath('//*[@id="stream-items-id"]/div/span')
+			image_urls = []
+			spans.each do |span|
+				image_urls << span.get_attribute('data-resolved-surl-mall')
+			end
 		elsif content_type == 'text/json' do
-			
+			parsed = JSON.parse(request.body)
+			html = Nokogiri::HTML(parsed['items_html'])
+			html.xpath('/span')
 		end
 	end
 
